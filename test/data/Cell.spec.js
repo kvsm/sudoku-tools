6747b84bf3b5790f3b7ecabb7b912a9bf199b623
Kevin Smith <kvsm81@gmail.com>
2016-11-12 01:35:30 +0000

    Added spec for parse()

--- test/data/Cell.spec.js
+++ test/data/Cell.spec.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import Cell from '../../src/data/Cell'
 
-test('isResolved()', t => {
+test('isResolved', t => {
   const cell1 = new Cell(1, [1])
   t.true(cell1.isResolved, 'cell with 1 value is resolved')
   const cell2 = new Cell(2, [1, 2, 3])
--- test/data/Puzzle.spec.js
+++ test/data/Puzzle.spec.js
@@ -1,8 +1,32 @@
 import test from 'ava'
+import {wrongLengthInput, notNumericInput, invalidStateInput, invalidPuzzle, validCompletePuzzle, validIncompletePuzzle} from '../fixtures'
 import Puzzle from '../../src/data/Puzzle'
-import {validCompletePuzzle, validIncompletePuzzle} from '../fixtures'
 
-test('isComplete()', t => {
-  t.true(validCompletePuzzle.isComplete, 'it is complete')
-  t.false(validIncompletePuzzle.isComplete, 'it is not complete')
+test('parse(str)', t => {
+  t.throws(() => {
+    Puzzle.parse()
+  }, 'Invalid state: No state specified',
+    'throws Error when no string is specified')
+  t.throws(() => {
+    Puzzle.parse(wrongLengthInput)
+  }, 'Invalid state: only standard 9x9 sudokus are supported, so state should be 81 characters long but was: 3',
+    'throws Error when state string is wrong length')
+  t.throws(() => {
+    Puzzle.parse(notNumericInput)
+  }, 'Invalid state: state should only contain digits 0-9',
+    'throws Error when state string is not numeric')
+  t.throws(() => {
+    Puzzle.parse(invalidStateInput)
+  }, 'Invalid state: breaks rules of sudoku',
+    'throws Error when state string is not a valid sudoku state')
+})
+
+test('isComplete', t => {
+  t.true(validCompletePuzzle.isComplete, 'complete puzzle is complete')
+  t.false(validIncompletePuzzle.isComplete, 'incomplete puzzle is not complete')
+})
+
+test('isValid', t => {
+  t.true(validCompletePuzzle.isValid, 'valid puzzle is valid')
+  t.false(invalidPuzzle.isValid, 'invalid puzzle is not valid')
 })
