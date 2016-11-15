a0d5ba74c21fce004870b28d8a7f9d488cd1de0a
Kevin Smith <kvsm81@gmail.com>
2016-11-15 21:16:11 +0000

    Improved test coverage for utils

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,11 +1,36 @@
 import test from 'ava'
-import {getResolvedCells, getResolvedCount} from '../src/utils'
+import * as Utils from '../src/utils'
 import {validIncompletePuzzle, validIncompleteRegion} from './fixtures.js'
 
+const invalidRegion = [
+  { values: [1] },
+  { values: [1] },
+  { values: [2] },
+  { values: [2] },
+  { values: [3] },
+  { values: [4] },
+  { values: [5] },
+  { values: [6] },
+  { values: [7] }
+]
+
 test('getResolvedCells', t => {
-  t.is(getResolvedCells(validIncompleteRegion).length, 2, 'returns only cells which are resolved')
+  t.is(Utils.getResolvedCells(validIncompleteRegion).length, 2, 'returns only cells which are resolved')
 })
 
 test('getResolvedCount', t => {
-  t.is(getResolvedCount(validIncompletePuzzle), 80, 'returns the correct number of resolved cells')
+  t.is(Utils.getResolvedCount(validIncompletePuzzle), 80, 'returns the correct number of resolved cells')
+})
+
+test('getStateFromRegions', t => {
+  const regions = Utils.getBlocksFromState(validIncompletePuzzle.currentState)
+  const result = Utils.getStateFromRegions(regions)
+  t.is(result.length, 81, 'returns state with the correct number of cells')
+  t.is(result[0].values[0], 1, 'has correct value in first cell')
+  t.is(result[80].values.length, 9, 'has all values in last cell')
+})
+
+test('validateRegion', t => {
+  t.true(Utils.validateRegion(validIncompleteRegion), 'valid region returns true')
+  t.false(Utils.validateRegion(invalidRegion), 'invalid region returns false')
 })
