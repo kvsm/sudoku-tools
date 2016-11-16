31a3414e41df0cd7b7cd7d416f4a111f94952c8f
Kevin Smith <kvsm81@gmail.com>
2016-11-16 22:41:33 +0000

    Changed validInput to improve coverage

--- test/fixtures.js
+++ test/fixtures.js
@@ -3,12 +3,13 @@ import Puzzle from '../src/data/Puzzle'
 export const wrongLengthInput = '123'
 export const notNumericInput = '12345678912345678912345678912345678912345678912345678912345678912345678912345678a'
 export const invalidStateInput = '111111789456789123789123456234567891567891234891234567345678912678912345912345670'
-export const validInput = '123456789456789123789123456234567891567891234891234567345678912678912345912345670'
+export const validInput = '123456789456789123789123456234567891567891234891234567345678912678912345912345600'
 export const invalidPuzzle = Puzzle.parse(validInput)
 invalidPuzzle.currentState[1].values = [1]
 export const validIncompletePuzzle = Puzzle.parse(validInput)
 export const validCompletePuzzle = Puzzle.parse(validInput)
 validCompletePuzzle.currentState[80].values = [8]
+validCompletePuzzle.currentState[79].values = [7]
 export const validIncompleteRegion = [
   { values: [1] },
   { values: [2] },
