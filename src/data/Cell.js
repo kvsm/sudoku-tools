be1cf6cb70525e2d98dd02445ec46206b18cf83b
Kevin Smith <kvsm81@gmail.com>
2016-11-16 22:37:12 +0000

    Added clone() as static method

--- src/data/Cell.js
+++ src/data/Cell.js
@@ -1,4 +1,12 @@
 export default class Cell {
+  static clone (cell) {
+    if (cell instanceof Cell) {
+      return new Cell(cell.index, cell.values)
+    } else {
+      throw new Error('Cell.clone(arg): arg is not an instance of Cell')
+    }
+  }
+
   constructor (index, values) {
     this.index = index
     this.values = values
